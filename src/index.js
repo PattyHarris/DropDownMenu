8059e8bcb22496d3403269316ec1216aaecbeb2f
PattyHarris <PattyHarris@users.noreply.github.com>
2022-01-26 11:37:36 -0800

    Step closer to showing when submenu is active.

--- src/index.js
+++ src/index.js
@@ -7,14 +7,21 @@ function hideSubmenu() {
     const submenu = document.getElementsByClassName("menuSub")[0];
     submenu.style.display = "none";
 }
-  
+/*
+    Sets the background when the menu is active.
+*/
+function onMenuItemMouseEnter(item) {
+    item.classList.add("menuMainItemActive");
+    showSubmenu();
+}
+
 document.addEventListener('DOMContentLoaded', (event) =>  {
 
     const menuItems = document.getElementsByClassName("menuMainItem");
     for (const menuItem of menuItems) {
-      menuItem.onmouseenter = showSubmenu
+      menuItem.onmouseenter = () => onMenuItemMouseEnter(menuItem);
     }
-    
+
     const menu = document.getElementsByClassName("menu")[0];
     menu.onmouseleave = hideSubmenu
 })
